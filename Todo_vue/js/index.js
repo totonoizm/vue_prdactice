a0de46d92a93f27ed7c8e926bdf70536282d03f0
Takeshi Ota <singer.scottie@gmail.com>
2021-04-29 09:24:19 +0900

    finish todo

--- Todo_vue/js/index.js
+++ Todo_vue/js/index.js
@@ -1,4 +1,26 @@
 new Vue({
     el:"#app",
-    data:{message:"helo"}
+    data:{
+        new_item:"",
+        todos:[]
+    },
+    methods: {
+        add_item: function(){
+            if(this.new_item == "") return; //バリデーション　空欄での追加を防ぐ
+            // alert("やることリストに" + this.new_item + "を追加しました"),
+            todo = {
+                item: this.new_item,
+                check:false
+            };
+            this.todos.push(todo) //入力
+            this.new_item = "" //入力欄のクリア
+
+        },
+        delete_item: function(index){ //index でタスクの順番を取得            
+            this.todos.splice(index, 1)
+        }
+
+
+    },
+
 })
